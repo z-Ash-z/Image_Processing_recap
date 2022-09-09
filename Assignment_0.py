bb0810af4cc35df16dfb77fc2e4779e4546afeb6
Aneesh Chodisetty <ch.aneesh1996@gmail.com>
2022-09-09 03:24:15 -0400

    cleanup and testing

--- Assignment_0.py
+++ Assignment_0.py
@@ -2,17 +2,17 @@ from utils.ImageProcessing import ImageAlgo
 
 if __name__ == '__main__':
     """
-    The main code for all the problems in assignment-0
+    The main code for all the problems in assignment-0 in CMSC733
     Author: Aneesh Chodisetty
+    Date created: 09/09/2022
     """
+    # Setting the input and the results path
     images_path = 'source/'
     results_path = 'results/'
-
     im = ImageAlgo(images_path, results_path)
-    im.readImage('iribefront.jpg')
-    # im.showImage()
 
-    # im.plotScanLine(250)
-    # print(im.image_matrix.shape)
-    # print(len(im.image_matrix[250, :, 0]))
+    # Reading the given image 
+    im.readImage('iribefront.jpg')
+    
+    # Problem - 1, plotting the RGB values along the scanline
     im.plotScanLine(250, '1_scanline.png')
